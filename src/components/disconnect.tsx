0567bc8e3592e3393c2e187d285f4463bf732d5e
unknown <ballogggamer@gmail.com>
2024-02-08 13:09:42 -0300

    solving component appearance problem

--- src/components/disconnect.tsx
+++ src/components/disconnect.tsx
@@ -1,16 +1,23 @@
-import { detectDisconnection,disconnection } from "../websocket/disconnected"
-import { useEffect } from "react"
+import { detectDisconnection } from "../websocket/disconnected"
+import { useEffect, useState } from "react"
 import "../css/disconnect.css"
 
+type DISCONNECTION = {
+    url: string,
+    state: boolean
+}
+
 const Disconnect = () => {
+    let [disconnection,setDisconnection] = useState<DISCONNECTION>(detectDisconnection())
 
     useEffect(() => {
-        detectDisconnection()
-    })
+        if (disconnection.state) 
+            console.log("disconnected:",disconnection)
+    },[disconnection.state])
 
     return (<>
         <p
-        hidden={disconnection.state}
+        hidden={!disconnection.state}
         id="disconnect_p"
         >
         the website can't communicate with the server.
--- src/websocket/disconnected.ts
+++ src/websocket/disconnected.ts
@@ -1,24 +1,18 @@
 import ws from "./connect";
 
-type DISCONNECTION = {
-    url: string,
-    state: boolean
-}
-
-let disconnection:DISCONNECTION = {
-    state: false,
-    url: ""
-}
-
 function detectDisconnection () {
+    let infoNetwork = {
+        url: "",
+        state: false
+    }
+
     if (ws.readyState == 3) {
         console.log("lost connection")
-        disconnection.state = true
-        disconnection.url = ws.url
-    } else {
-        disconnection.state = false
-        disconnection.url = ""
+        infoNetwork.state = true
+        infoNetwork.url = ws.url
     }
+
+    return infoNetwork
 }
 
-export {detectDisconnection, disconnection}
+export {detectDisconnection}

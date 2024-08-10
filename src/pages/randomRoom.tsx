98d24c5f64cbee899cf1924356b32e692c3773ee
unknown <ballogggamer@gmail.com>
2024-08-10 13:03:38 -0300

    add random room style

--- src/pages/randomRoom.tsx
+++ src/pages/randomRoom.tsx
@@ -1,24 +1,51 @@
 import { useEffect, useState } from "react"
 import { usePage } from "../types/page"
+import "../css/randomRoom.css"
+import { useStatus } from "../types/playerStatus"
+import send from "../websocket/send"
 
 const RandomRoom = () => {
     const page = usePage()
+    const status = useStatus()
     const imgs = ["circle.svg", "person.png", "close.svg"]
     const [indexImg, setIndexImg] = useState(0) 
+    const [activeAnimation, setActiveAnimation] = useState(true)
+    const [activeRandomRoom, setActiveRandomRoom] = useState(false)
 
     useEffect(() => {
         page.setName("RANDOM ROOM")
         page.setOpenBars(true)
+        setActiveRandomRoom(status.randomRoom)
+        
+        if (status.randomRoom) {
+            send({
+                type: "invite-by-random-room"
+            })
+        }
     },[])
 
     return (<div className="random_room">
-        <div className="icon_loading">
-            <img src={imgs[indexImg]} alt="" />
-        </div>
+        {
+            activeRandomRoom 
+            &&
+            <div className="icon_loading">
+                <img style={{animationName: (activeAnimation ? "scaleIt" : "")}} src={imgs[indexImg]} alt="" />
+            </div> 
+        }
 
-        <p className="someone">{"fwag"}</p>
+        <p className="someone">
+            {(activeRandomRoom ? 
+                "gwagoanwifa"
+                :
+                "A opção random room está desativada. Ative-a para poder usar o random room." 
+            )}
+        </p>
 
-        <p className="someone">{"feshse"}</p>
+        {
+            activeRandomRoom 
+            &&
+            <p className="someone">{"feshse"}</p>
+        }
     </div>)
 } 
 
